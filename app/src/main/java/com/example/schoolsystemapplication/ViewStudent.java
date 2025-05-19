ee7ace5a732bcff0c245d074be5befb9e0341a11
Yara Hamad <yara.amjadhamad04@gmail.com>
2025-05-19 13:28:48 +0300

    Screens I created for the manager

--- app/src/main/java/com/example/schoolsystemapplication/ViewStudent.java
+++ app/src/main/java/com/example/schoolsystemapplication/ViewStudent.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.schoolsystemapp.R;
+
 public class ViewStudent extends AppCompatActivity {
 
     @Override
