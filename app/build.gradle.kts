92dcfc0a3c3f6654621bf7ba0a4e7a32cfb74da4
lanazaben <lanazaben68@gmail.com>
2025-05-24 01:04:05 +0300

    students marks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.schoolsystemapp"
+        applicationId = "com.example.schoolsystemapplication"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
--- app/src/main/java/com/example/schoolsystemapplication/MainActivity.java
+++ app/src/main/java/com/example/schoolsystemapplication/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.schoolsystemapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
